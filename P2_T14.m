c18fbd5bcc41071ce9863052c684e85732b81b43
Mahmoud Lotfy <142970553+MahmoudLotfy10@users.noreply.github.com>
2024-04-17 20:50:53 +0200

    Update P2_T14.m

--- P2_T14.m
+++ P2_T14.m
@@ -8,7 +8,7 @@ data = randi ([0,1],1,n_bits);
 polar_data = (2*data)-1;
 data_upsampled = upsample(polar_data,n_samples);
 y = conv(data_upsampled, p);
-
+ 
 figure;
 subplot(4,1,1);
 plot(p);
@@ -16,25 +16,25 @@ grid on;
 title("pulse shaping function");   
 xlabel("Time");
 ylabel("Amplitude");
-
+ 
 subplot(4,1,2);
 plot(data_upsampled);
 grid on;
 title("Data Upsampled");   
 xlabel("Time");
 ylabel("Amplitude");
-
+ 
 subplot(4,1,3);
 plot(y);
 grid on;
 title("Transmitted signal"); 
 xlabel("Time");
 ylabel("Amplitude");
-
+ 
 %%%%%%%%%%%%%%%%%%%%  Filters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 matched_filter = fliplr(p);
 non_matched_filter = [1 1 1 1 1]/sqrt(5);
-
+ 
 %%%%%%%%%%%%%%%%%%%%% output of filters%%%%%%%%%%%%%%%%%%%%%%
 matched_filter_out = conv(y,matched_filter);
 non_matched_filter_out = conv(y,non_matched_filter);
@@ -50,10 +50,10 @@ ylabel("Amplitude");
 %to adjust length
 matched_filter_out = matched_filter_out(1:length(train_impulses));
 non_matched_filter_out = non_matched_filter_out(1:length(train_impulses));
-
+ 
 matched_filter_out_s     = matched_filter_out .* train_impulses ;
 non_matched_filter_out_s = non_matched_filter_out .* train_impulses ;
-
+ 
 correlator_out = zeros(1, length(y) - length(p) + 6);
 for i = 1:5:length(y) - length(p) + 1
     out = 0;
@@ -63,8 +63,8 @@ for i = 1:5:length(y) - length(p) + 1
     end
 end
 corr_out_s = correlator_out .* train_impulses ;
-
-
+ 
+ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure('Name','Filters');
 subplot(2,1,1);
@@ -79,7 +79,7 @@ grid on
 xlabel('time');
 ylabel('amplitude');
 title('non-matched filter');
-
+ 
 figure('Name','output of the filters and after sample');
 x_rescaled = (1:length(matched_filter_out)) / 5;
 subplot(2,1,1);
@@ -92,7 +92,7 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('matched filter output');
 legend 'matched filter output' 'matched filter output sampled';
-
+ 
 subplot(2,1,2);
 plot(x_rescaled,non_matched_filter_out,'r');
 hold on;
@@ -103,7 +103,7 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('matched filter output');
 legend 'non matched filter output' 'non matched filter output sampled';
-
+ 
 figure('Name','Comparison between the outputs of the filters at the sampling instants');
 subplot(2,1,1);
 plot(x_rescaled,matched_filter_out,'r');
@@ -123,7 +123,7 @@ grid on
 xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('non-matched filter output');
-
+ 
 figure('Name',' Comparison between the outputs of the filters after sampling');
 plot(x_rescaled,matched_filter_out_s,'r-');
 hold on;
@@ -134,7 +134,7 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('Comparison between the outputs of the filters after sampling ');
 legend 'matched filter' 'non matched filter';
-
+ 
 figure('Name','output of matched and correlator');
 subplot(2,1,1);
 plot(x_rescaled,matched_filter_out,'r');
@@ -146,7 +146,7 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('matched filter output');
 legend 'matched filter output' 'matched filter output sampled';
-
+ 
 subplot(2,1,2);
 plot(x_rescaled,correlator_out,'r');
 hold on;
@@ -157,8 +157,8 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('matched filter output');
 legend 'correlator output' 'correlator output sampled';
-
-
+ 
+ 
 figure('Name','output of matched and correlator after sampling');
 plot(x_rescaled,matched_filter_out_s,'r-');
 hold on;
@@ -169,33 +169,33 @@ xlabel('Time [Ts sec]');
 ylabel('Amplitude');
 title('output of matched and correlator after sampling');
 legend 'matched filter' correlator;
-
+ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%% Noise Analysis && Requirement 2 %%%%%%%%%%%%%%%%%%%
-
+ 
 n_bits = 10^4;
 n_samples = 5;
 data = randi ([0,1],[1,n_bits]);
 polar_data = (2*data)-1;
 data_upsampled = upsample(polar_data,n_samples);
-
+ 
 p = [5 4 3 2 1]/sqrt(55);
-
+ 
 y = conv(data_upsampled, p);
 y = y(y~=0);
-
+ 
 unity_noise = randn(1,5*n_bits);
-
+ 
 matched_filter = fliplr(p);
 non_matched_filter_2 = [5 5 5 5 5]/sqrt(125);
-
+ 
 snr = -2:1:5;
-
+ 
 BER_theoretical = zeros(size(snr));
 BER_1 = zeros(size(snr));
 BER_2 = zeros(size(snr));
-
+ 
 for i=snr
     index=i+3;
     no=1/db2mag(i);
@@ -232,7 +232,7 @@ for i=snr
     BER_theoretical(index) = 0.5*erfc(sqrt(1/no));
     
 end
-
+ 
 figure("name" , 'BER plot');
 subplot(2,2,1);
 semilogy(snr,BER_1); 
@@ -240,21 +240,21 @@ title("BER matched filter");
 grid on;
 xlabel("Eb/No");
 ylabel("BER");
-
+ 
 subplot(2,2,2); 
 semilogy (snr,BER_2);
 title("BER non matched filter");   
 grid on;
 xlabel("Eb/No");
 ylabel("BER");
-
+ 
 subplot(2,2,3);
 semilogy(snr,BER_theoretical);
 title("BER theoretical"); 
 grid on;
 xlabel("Eb/No");
 ylabel("BER");
-
+ 
 subplot(2,2,4);
 semilogy(snr,BER_theoretical,'b');
 hold on;
@@ -266,23 +266,21 @@ legend 'BER theoretical' 'BER matched filter' 'BER non matched filter';
 grid on;
 xlabel("Eb/No");
 ylabel("BER");
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%% ISI && Requirement 3 %%%%%%%%%%%%%%%%%%%
-
-n_bits = 100; 
+ n_bits = 100; 
 data = randi([0, 1], [1, n_bits]);
 polar_data = (2 * data) - 1;
 data_upsampled = upsample(polar_data, n_samples);
-
+ 
 R_cases = [0, 0, 1, 1];
 delay_cases = [2, 8, 2, 8];
-
 figure("Name", 'rcosine filter');
 for i = 1:4 
     R = R_cases(i);
     delay = delay_cases(i);
-    
+   
     % the square root raised cosine filter
     filter = rcosine(1, n_samples, 'sqrt', R, delay);
     figure(9);
@@ -292,15 +290,19 @@ for i = 1:4
     xlabel("time samples");
     ylabel("Amplitude");
         
-    transmit_data = conv(data_upsampled, filter);
-    receive_data = conv(transmit_data, filter);  
+    transmit_data = conv(data_upsampled, filter,'same');
+    receive_data = conv(transmit_data, filter,'same');  
     
     % Ensure transmit_data and receive_data have the same length
     min_length = min(length(transmit_data), length(receive_data));
     transmit_data = transmit_data(1:min_length);
     receive_data = receive_data(1:min_length);
     
-    eye_diagram_data=[transmit_data; receive_data];
-    eye_diagram=eyediagram(eye_diagram_data', n_samples * 2);
-    set(eye_diagram,'Name',"Eye Diagram when R="+R_cases(i)+" & delay="+delay_cases(i)+"");
+    eye_diagram_data = [transmit_data ; receive_data];
+ 
+    eye_diagram_I = eyediagram(eye_diagram_data(1,:), n_samples * 2); % In-phase
+    title("Eye Diagram for In-Phase Signal when R="+R_cases(i)+" & delay="+delay_cases(i)+"");
+ 
+    eye_diagram_Q = eyediagram(eye_diagram_data(2,:), n_samples * 2); % Quadrature
+    title("Eye Diagram for Quadrature Signal when R="+R_cases(i)+" & delay="+delay_cases(i)+"");
 end
